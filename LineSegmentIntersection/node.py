a8ce4198b0a42f0bf1711b81ee649898107ae179
Debanjan Dey <debanjandey64@gmail.com>
2021-07-10 21:47:53 +0530

    Delete LineSegmentIntersection directory

--- LineSegmentIntersection/node.py
+++
@@ -1,8 +0,0 @@
-class node(object):
-
-    def __init__(self,data,parent):
-        self.data=data
-        self.leftchild=None
-        self.rightchild=None
-        self.balance=0
-        self.parent=parent
