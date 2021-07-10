b09413a30d70a1de405f5871584fd16973175cb8
Debanjan Dey <debanjandey64@gmail.com>
2021-07-10 22:12:47 +0530

    Delete msgbx.py

--- msgbx.py
+++
@@ -1,8 +0,0 @@
-from tkinter import *
-
-ms=Tk()
-msq = "The condition necessary in the taught are purity, a real thirst after knowledge, and perseverence.\n -Swami Vivekananda"
-msg = Message(ms,text =msq)
-msg.config(bg='orange',font=('ms sans serif',20,'normal'))
-msg.pack()
-mainloop()
