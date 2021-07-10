6f94e07c08ea864e677d14f92f313a1ff85313f0
Debanjan Dey <debanjandey64@gmail.com>
2021-07-10 22:30:41 +0530

    Delete tfdem1.py

--- tfdem1.py
+++
@@ -1,8 +0,0 @@
-import nltk
-from nltk.tokenize import sent_tokenize, word_tokenize
-
-#nltk.download('punkt')
-example_text = "Hello Mr. Dhalu, how are you doing today? The weather is great and Python is awaawa. The sky is green. You should not eat cardboard"
-
-print(sent_tokenize(example_text))
-print(sent_tokenize(example_text))
